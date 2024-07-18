012a3f63b8ca00782b982a31d135eeba7063d73f
Pratik-Abhang <147050620+Pratik-Abhang@users.noreply.github.com>
2024-07-19 00:11:50 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -1,19 +1,36 @@
 import streamlit as st
-import pickle
+import pickle 
 
 st.title("Battle of texts: AI vs Human Text Detection")
 st.subheader("Enter the text")
 
+
 input_text = st.text_input("")
 
-# Load the saved pipeline
-with open('text_classification_pipeline.pkl', 'rb') as f:
-    pipeline = pickle.load(f)
+# Load the saved components
+with open('tfidf_vectorizer.pkl', 'rb') as f:
+    loaded_tfidf = pickle.load(f)
 
-# Transform and predict using the pipeline
+with open('truncated_svd.pkl', 'rb') as f:
+    loaded_svd = pickle.load(f)
+
+with open('svc.pkl', 'rb') as f:
+    loaded_model = pickle.load(f)
+
+# Function to preprocess text using TF-IDF and SVD
+def preprocess_text(text, vectorizer, svd):
+    # Vectorize input text using TF-IDF
+    text_tfidf = vectorizer.transform([text])
+
+    # Reduce dimensionality using SVD
+    text_svd = svd.transform(text_tfidf)
+
+    return text_svd
+
+# Preprocess input text and make prediction
 if input_text:
-    # Transform the input text and make prediction
-    prediction_result = pipeline.predict([input_text])
+    input_svd = preprocess_text(input_text, loaded_tfidf, loaded_svd)
+    prediction_result = loaded_model.predict(input_svd)
     
     # Display the prediction result
     st.write("Prediction result:", prediction_result)
