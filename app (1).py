f646725ed8e12d6a96694914d6140983fe1a1b88
Pratik-Abhang <147050620+Pratik-Abhang@users.noreply.github.com>
2024-07-18 15:19:57 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -14,32 +14,35 @@ st.title("AI vs human")
 st.subheader("Enter the text")
 
 input_text = st.text_input("")
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer()
+input_text = vectorizer.fit_transform(['input_text'])
+ 
 
+#dt=pd.read_csv("AI_Human.csv")
+#st.write(dt.head())
 
-dt=pd.read_csv("AI_Human.csv")
-st.write(dt.head())
+# dt["len"]=dt["text"].apply(len)
+# ai=dt[dt['generated']==1]
+# human=dt[dt['generated']==0]
 
-dt["len"]=dt["text"].apply(len)
-ai=dt[dt['generated']==1]
-human=dt[dt['generated']==0]
-
-dt['generated_cor'] = dt['generated'].replace({
-    1: 'AI',
-    0: 'Human'
-})
+# dt['generated_cor'] = dt['generated'].replace({
+#     1: 'AI',
+#     0: 'Human'
+# })
 
 #spliting data
 
-X_train,x_test,y_train,y_test=train_test_split(dt['text'],dt['generated_cor'],test_size=0.2,random_state=42)
+#X_train,x_test,y_train,y_test=train_test_split(dt['text'],dt['generated_cor'],test_size=0.2,random_state=42)
 
-from sklearn.pipeline import Pipeline
+# from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB, ComplementNB
-from sklearn.svm import LinearSVC
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.model_selection import learning_curve
-from sklearn.linear_model import LogisticRegression
+# from sklearn.naive_bayes import MultinomialNB, ComplementNB
+# from sklearn.svm import LinearSVC
+# from sklearn.svm import SVC
+# from sklearn.metrics import accuracy_score, classification_report
+# from sklearn.model_selection import learning_curve
+# from sklearn.linear_model import LogisticRegression
 
 #load the model
 
